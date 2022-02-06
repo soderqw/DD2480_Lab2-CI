a6a45c3a911dd8fad5324e91c20a974abbae6e06
Oscar <53902126+oscarzhpersson@users.noreply.github.com>
2022-02-06 22:58:38 +0100

    Issue#7 Adds functionality to clone repo (#17)
    * FIX - Issue#7: Adds better debug messages for testing the web server
    
    * REFAC - Issue#7: Fixes order of functions within main.py
    
    * feat - Issue#7: Adds an improved debug message
    
    Aims to test if one can print out messages from the terminal running Flask
    
    * FIX - Issue#7: fixes print structure
    
    Mostly just to debug the push webhook
    
    * FIX - Issue#7: Adds debug logging via the APP interface to terminal
    
    * FIX - Issue#7: Adds a working basic webserver implementation
    
    The previous implementation had some errors which this fixes
    
    * FEAT - Issue#7: Separates the properties of the web server from the code
    
    * REFAC - Issue#7: Cleans up unecessary packages
    
    * DOC - Issue#7: Adds comments to the main.py file
    
    * DOC - Issue#7: Adds documentation to comments in main.py. Cleans up stale comments
    
    * FEAT - Issue#7: Begins data extraction from payload
    
    * Issue#7 Basic Web server Skeleton (#12)
    
    * FEAT - Issue#7: Adds project structure and setup files
    
    * feat - Issue#7: Adds ngrok program to .gitignore file
    
    * feat - Issue#7: Adds basic skeleton for GitHub webhook application
    
    * FIX - Issue#7: Adds better debug messages for testing the web server
    
    * REFAC - Issue#7: Fixes order of functions within main.py
    
    * feat - Issue#7: Adds an improved debug message
    
    Aims to test if one can print out messages from the terminal running Flask
    
    * FIX - Issue#7: fixes print structure
    
    Mostly just to debug the push webhook
    
    * FIX - Issue#7: Adds debug logging via the APP interface to terminal
    
    * FIX - Issue#7: Adds a working basic webserver implementation
    
    The previous implementation had some errors which this fixes
    
    * FEAT - Issue#7: Separates the properties of the web server from the code
    
    * REFAC - Issue#7: Cleans up unecessary packages
    
    * DOC - Issue#7: Adds comments to the main.py file
    
    * DOC - Issue#7: Adds documentation to comments in main.py. Cleans up stale comments
    
    * FIX - Issue#7: Changes PORT to adhere to assignment specification (5000 --> 8080)
    
    * FEAT - Issue#7: Adds important libraries for furrher progress (os, sys)
    
    * FEAT - Issue#7: Adds functionality to grab clone_URL from the payload in main.py
    
    * FEAT - Issue#7: Adds functionality to clone git repository in CWD or else specified
    
    * FIX - Issue#7: Casts os.getcwd to string before changing directory
    
    * FIX - Issue#7: Removes addition to cwd in main.py
    
    * FIX - Issue#7: Changes os.getcwd to os.getcwd()
    
    * FEAT - Issue #7: Adds mac directory meta data files to .gitignore

--- continousIntegration/main.py
+++ continousIntegration/main.py
@@ -1,25 +1,51 @@
 ##### IMPORTS #####
 
 from flask import Flask, request, json
+import os
+import sys
+
+##### SETTINGS #####
+
+STATUS_DEBUG = True
+HOST = "127.0.0.1"
+PORT = 8080
+
+PATH_REPO = str(os.getcwd()) # Folder within CWD to run the code.
 
 ##### PROGRAM #####
 
-app = Flask(__name__)
-
-@app.route('/') # Triggered by URL localhost:5000/
-def start():
-    return 
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+app = Flask(__name__) # Variable for flask server application, to be called upon.
 
 # Create an endpoint which receives requests from the GitHub API.
-
-@app.route('/githubPush', methods=['POST']) # Triggered by URL localhost:5000/githubPush
+@app.route('/', methods=['POST']) # Triggered by URL localhost:5000/
 def handler_Push():
-    data = request.json
 
-    print("Tests if webhook event is received.")
+    data = request.json # Request the data from the event.
 
-    return data
+    print("Received PUSH event from webhook!") # Debug print.
+
+    # TODO: Extract Repository.
+    # TODO: Save relevant parts of repo into variables to send to the other scripts.
+
+    # Here you do all the continuous integration tasks
+    # For example:
+    # 1st clone your repository
+    # 2nd compile the code
+
+    # Step 1: Clone the repository.
+    branch = data["repository"]["clone_url"] # Fetches the clone URL from the payload.
+
+    #os.chdir(str(os.getcwd) + PATH_REPO) # Changes current directory to where the cloned repository is to be located.
+    os.chdir(PATH_REPO)
+    os.system("git clone " + branch) # Runs command to clone the repository.
+
+    # TODO: Run module that compiles.
+    # TODO: Run module that tests.
+
+    #Flask.Response(status=200)
+    return "OK" # Defaults to 200 response code.
+
+
+# Start the Flask web server.
+if __name__ == '__main__':
+    app.run(debug=STATUS_DEBUG, host=HOST, port=PORT)
