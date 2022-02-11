f647b4be7401230271fb4d31e92bb000a3aeb1fe
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-02-11 11:55:10 +0100

    ADDS - Issue #23: Adds new functionality that creates a log file for each push

--- continousIntegration/modules/logger.py
+++ continousIntegration/modules/logger.py
@@ -19,16 +19,24 @@ def logger(PATH_REPO, name, message, sender, sha):
 
 
     #Checks if the file exist
-    file_exists = os.path.exists('../../../logging.txt') #will hold a True or False value
-
+    p = f"../../../logging{sha}.txt"
+    file_exists = os.path.exists(p) #will hold a True or False value
+    print(file_exists)
     #Creates the file if it does not exist
-    if(file_exists == False):
-        f = open("../../../logging.txt", "x")
+    if (file_exists == False):
+        f = open(p, "w")
 
+    print(file_exists)
     # Append-adds at last of the file
     time = datetime.now()
     path = PATH_REPO + '/' + name
-
-    file1 = open("../../../logging.txt", "a")  #append mode
+    print("RUNNING")
+    file1 = open(p, "w")  #append mode
     file1.write("Push event from: " + sender + "\n" + "Path: " + path + "\n" + "Compiled at:" + str(time) + "\n" + "Status: " + message + "\n" + "Sha: " + sha + "\n\n")
     file1.close()
+
+def main():
+    logger("Musse/Tim/wille", "ISSUE#33", "FAIL", "MUSSE", "dasadssads")
+
+if __name__ == "__main__":
+    main()
